1412d580e503d3f3ff39854cfc87d408dcfcc5f4
McCallWang <1059511459@qq.com>
2020-09-14 11:44:24 +0800

    可以保存（存在 BUG，数据重复，不覆盖，按键跳转无效！）

--- main.js
+++ main.js
@@ -1,46 +1,48 @@
 const keys = document.querySelectorAll('.key')
 let sites = window.localStorage.getItem("siteStorage")
 let sitesObject = JSON.parse(sites)
-let hashMap = [
-    {
-        id: 0,
-        key: 'g',
-        url: 'https://www.google.com',
-        logo: './images/谷歌.png'
-    },
-    {
-        id: 1,
-        key: 'b',
-        url: 'https://www.bilibili.com',
-        logo: './images/哔哩哔哩.png'
-    },
-]
+let hashMap = sitesObject
 
-hashMap.forEach((node, i) => {
-    keys.forEach((key, j) => {
-        key.addEventListener('mousedown', () => {
-            key.classList.add('shadow-down')
+
+const website = document.querySelector('.website')
+const submit = document.querySelector('.submit')
+const select = document.querySelector('.abc')
+let userUrl = ""
+let userKey = ""
+
+const mask = document.querySelector('.mask')
+const closeElement = document.querySelector('.close')
+const cancel = document.querySelector('.cancel')
+const setting = document.querySelector('.settings')
+
+const render = () => {
+    hashMap.forEach((node, i) => {
+        keys.forEach((key, j) => {
+            key.addEventListener('mousedown', () => {
+                key.classList.add('shadow-down')
+            })
+            key.addEventListener('mouseup', () => {
+                key.classList.remove('shadow-down')
+            })
+            key.addEventListener('mouseout', () => {
+                key.classList.remove('shadow-down')
+            })
+            if (node.key === key.innerText) {
+                const img = document.createElement('img')
+                img.src = node.logo
+                key.appendChild(img)
+            }
         })
-        key.addEventListener('mouseup', () => {
-            key.classList.remove('shadow-down')
-        })
-        key.addEventListener('mouseout', () => {
-            key.classList.remove('shadow-down')
-        })
-        if (node.key === key.innerText.toLowerCase()) {
-            const img = document.createElement('img')
-            img.src = node.logo
-            key.appendChild(img)
+        let xxx = (e) => {
+            if (e.key === node.key) {
+                window.open(node.url)
+                // location.assign(node.url)
+            }
         }
+        document.addEventListener('keypress', xxx)
     })
-    let xxx = (e) => {
-        if (e.key === node.key) {
-            window.open(node.url)
-            // location.assign(node.url)
-        }
-    }
-    document.addEventListener('keypress', xxx)
-})
+}
+render()
 
 window.onbeforeunload = () => {
     let string = JSON.stringify(hashMap);
@@ -48,56 +50,36 @@ window.onbeforeunload = () => {
 };
 
 
-
-
-
-
-
-
-
-
-
-const website = document.querySelector('.website')
-const submit = document.querySelector('.submit')
-const select = document.querySelector('.abc')
-
-
 submit.addEventListener('click', () => {
-    let url = ""
-    let key = ""
     const abc = select.options[select.selectedIndex].text
-    url = website.value
-    key = abc
+    userUrl = website.value
+    userKey = abc
+    hashMap.push({
+        id: 0,
+        key: userKey,
+        url: userUrl,
+        logo: userUrl + '/favicon.ico'
+    })
+    render()
+    close()
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-const setting = document.querySelector('.settings')
-setting.onclick = function () {
-    // document.removeEventListener('keypress', xxx)
+// 关闭 | 显示 弹窗
+function close() {
+    mask.style.display = 'none'
+}
+function show() {
     mask.style.display = 'block'
 }
-const close = document.querySelector('.close')
-const cancel = document.querySelector('.cancel')
-const mask = document.querySelector('.mask')
-close.addEventListener('click', () => {
-    mask.style.display = 'none'
+closeElement.addEventListener('click', () => {
+    close()
 })
 cancel.addEventListener('click', () => {
-    mask.style.display = 'none'
+    close()
+})
+setting.addEventListener('click', () => {
+    show()
 })
 
-
-
+console.log('hashMap:', hashMap)
