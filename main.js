a1bf4bc6858368a3e7364faa6dd0838fd432ce7d
McCallWang <1059511459@qq.com>
2020-09-15 12:35:20 +0800

    可以保存设置&&支持按键跳转&&BUG：图标网站还是重复

--- main.js
+++ main.js
@@ -11,72 +11,80 @@ const setting = document.querySelector('.settings')
 let sites = window.localStorage.getItem("siteStorage")
 let sitesObject = JSON.parse(sites)
 let hashMap = sitesObject || [
-    { id: '1', key: 'A', url: 'baidu.com', logo: 'noLogo' },
-    { id: '2', key: 'A', url: 'baidu2.com', logo: 'noLogo2' },
-    { id: '3', key: 'B', url: 'baidu3.com', logo: 'noLogo3' },
-    { id: '4', key: 'B', url: 'baidu4.com', logo: 'noLogo4' },
-    { id: '4', key: 'B', url: 'baidu4.com', logo: 'noLogo5' },
+    { id: 0, key: 'B', url: 'https://baidu.com', logo: '/favicon.ico' },
+    { id: 0, key: 'J', url: 'https://jingdong.com', logo: '/favicon.ico' },
 ]
-// 保证 key 不重复
-let uniqueHashMap = Object.values(
-    hashMap.reduce((uniqueHashMapMap, item) => {
-        uniqueHashMapMap[item.key] = item
-        return uniqueHashMapMap
-    }, [])
-)
 
 
 const render = () => {
     keys.forEach(key => {
-        // 得到 每个 按键元素  => 从而 获取到按键上的 大写字母 key.innerText
-        // 大写字母需要和 谁 对比？
         hashMap.forEach(node => {
-            // 得到 每个 {id,key,url,logo} 对象 ，也就是一个 node , node 中有 node.key 大写的
             if (key.innerText === node.key) {
+                // 删除 key 原有的子元素 ，再更新子元素。
                 while (key.hasChildNodes()) {
                     key.removeChild(key.firstChild)
                 }
                 key.innerText = node.key
                 const img = document.createElement('img')
-                img.src = node.url + node.logo   // 设置  img 的src 地址 用于展示 图片
+                img.src = node.url + node.logo
                 key.appendChild(img)
             }
-            key.addEventListener('mousedown', () => {
-                key.classList.add('shadow-down')
+            let openWebsite = (e) => {
+                console.log('e.key', e.key)   // 两层循环，导致了 log 出了 26*(e.key) 数量的东西
+                if (e.key === node.key.toLowerCase()) {
+                    window.open(node.url)
+                    // location.assign(node.url)
+                }
+            }
+            document.addEventListener('keypress', openWebsite, false)
+            website.addEventListener('input', () => {
+                document.removeEventListener('keypress', openWebsite)
             })
-            key.addEventListener('mouseup', () => {
-                key.classList.remove('shadow-down')
-            })
-            key.addEventListener('mouseout', () => {
-                key.classList.remove('shadow-down')
-            })
-            // let xxx = (e) => {
-            //     if (e.key === node.key) {
-            //         window.open(node.url)
-            //         // location.assign(node.url)
-            //     }
-            // }
-            // document.addEventListener('keypress', xxx)
-
+        })
+        key.addEventListener('click', (e) => {
+            const clickedElement = e.target
+            select.value = e.target.innerText
+            show()
         })
 
+        key.addEventListener('mousedown', () => {
+            key.classList.add('shadow-down')
+        })
+        key.addEventListener('mouseup', () => {
+            key.classList.remove('shadow-down')
+        })
+        key.addEventListener('mouseout', () => {
+            key.classList.remove('shadow-down')
+        })
     })
 }
 render()
 
 submit.addEventListener('click', () => {
+    userKey = select.value
     userUrl = website.value
-    userKey = select.options[select.selectedIndex].text
-    hashMap.forEach((node, index) => {
-
+    hashMap.push({
+        id: '3', key: userKey, url: userUrl, logo: '/favicon.ico'
     })
     render()
+    close()
 })
 
+
+
 window.onbeforeunload = () => {
+    // 保证 key 不重复
+    let uniqueHashMap = Object.values(
+        hashMap.reduce((uniqueHashMapMap, item) => {
+            uniqueHashMapMap[item.key] = item
+            return uniqueHashMapMap
+        }, [])
+    )
     let string = JSON.stringify(uniqueHashMap);
     window.localStorage.setItem("siteStorage", string);
 };
+
+
 
 // 关闭 | 显示 弹窗
 function close() {
